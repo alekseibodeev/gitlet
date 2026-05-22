d72f9f2a03a150a453528490e512e8cd37db2413
Aleksei Bodeev <bodeevaleksei@gmail.com>
2026-05-22 09:58:34 +0500

    test: Add failing 'checkout' test

---
+++ tests/test_checkout.py
@@ -0,0 +1,16 @@
+import os
+from pathlib import Path
+
+import gitlet
+
+
+def test_checkout_basic(tmp_path):
+    os.chdir(tmp_path)
+    gitlet.main(["gitlet", "init"])
+    file = Path("hello")
+    file.write_text("hello")
+    gitlet.main(["gitlet", "add", "hello"])
+    gitlet.main(["gitlet", "commit", "add hello"])
+    file.write_text("hola")
+    gitlet.main(["gitlet", "checkout", "--", "hello"])
+    assert file.read_text() == "hello"
